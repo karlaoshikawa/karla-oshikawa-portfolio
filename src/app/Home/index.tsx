ba35ccf93b53604fbf7cffc897824ca1fc31eada
Karla Oshikawa <karla_hara@hotmail.com>
2024-04-09 17:11:46 +0900

    config: initial setting

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+const Header: React.FC = () => {
+  return (
+    <div>
+    <h1>header</h1>
+    </div>
+  );
+};
+export default Header;
