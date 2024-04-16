b8d8c68af24036e35415afa1d00cc6ea6653b73d
Karla Oshikawa <karla_hara@hotmail.com>
2024-04-16 10:56:01 +0900

    feat: pag em jp e pt

--- src/app/pt/page.tsx
+++ src/app/pt/page.tsx
@@ -1,20 +1,9 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import AboutMe from "@/components/AboutMe";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
+import HomePage from "../HomePage";
 
 const Homept: React.FC = () => {
-  const pathname = usePathname();
-  const pathSegments = pathname.split("/");
-  const locale = pathSegments[1];
-
   return (
     <div>
-      <Header lang={locale} />
-      <Hero />
-      <AboutMe lang={locale} />
+      <HomePage />
     </div>
   );
 };
