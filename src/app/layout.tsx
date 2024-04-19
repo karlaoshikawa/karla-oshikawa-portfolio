286aaa0a5277cedcf183135a32ac520e64077033
Karla Oshikawa <karla_hara@hotmail.com>
2024-04-20 01:03:36 +0900

    style: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Karla Oshikawa - Portifolio",
+  title: "Karla Oshikawa - Portfolio",
   description:
     "Portfolio showcasing my ongoing web development projects and studies.",
 };
@@ -16,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className}>{children}</body>
-    </html>
+    </>
   );
 }
